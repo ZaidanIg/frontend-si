f738085f422c774387c76b7931452aaf12e3f65b
WildanFathir <wildanfathir612@gmail.com>
2024-11-08 19:56:01 +0700

    feat: update FooterLayout component with social media links and URLs

--- src/components/FooterLayout.vue
+++ src/components/FooterLayout.vue
@@ -6,15 +6,21 @@
       <h1 class="text-3xl font-semibold text-white">TEMUKAN KAMI DI</h1>
 
       <div class="flex items-center justify-center space-x-[100px]">
-        <a href="#" class="flex flex-col items-center space-y-[25px]">
+        <a
+          href="https://www.instagram.com/himsif.unsap?igsh=MmR5YTlnaGN2YWN5"
+          class="flex flex-col items-center space-y-[25px]"
+        >
           <img src="../assets/images/instagram.png" alt="instagram" width="75" height="75" />
           <p class="text-xl text-white">Instagram</p>
         </a>
-        <a href="#" class="flex flex-col items-center space-y-[25px]">
+        <a href="https://wa.me/+6287794711241" class="flex flex-col items-center space-y-[25px]">
           <img src="../assets/images/whatsapp.png" alt="whatsapp" width="75" height="75" />
           <p class="text-xl text-white">Whatsapp</p>
         </a>
-        <a href="#" class="flex flex-col items-center space-y-[25px]">
+        <a
+          href="https://www.facebook.com/profile.php?id=61568104543532&mibextid=ZbWKwL"
+          class="flex flex-col items-center space-y-[25px]"
+        >
           <img src="../assets/images/facebook.png" alt="facebook" width="75" height="75" />
           <p class="text-xl text-white">Facebook</p>
         </a>
