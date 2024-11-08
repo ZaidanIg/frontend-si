360a59d3e0400bd1ec5ea484274cadc7dafac72f
WildanFathir <wildanfathir612@gmail.com>
2024-11-08 17:01:39 +0700

    feat: add FooterLayout component with social media links

---
+++ src/components/FooterLayout.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="bg-[#787777]">
+    <footer
+      class="flex-col items-center justify-center text-center page-content space-y-[75px] py-[30px]"
+    >
+      <h1 class="text-3xl font-semibold text-white">TEMUKAN KAMI DI</h1>
+
+      <div class="flex items-center justify-center space-x-[100px]">
+        <a href="#" class="flex flex-col items-center space-y-[25px]">
+          <img src="../assets/images/instagram.png" alt="instagram" width="75" height="75" />
+          <p class="text-xl text-white">Instagram</p>
+        </a>
+        <a href="#" class="flex flex-col items-center space-y-[25px]">
+          <img src="../assets/images/whatsapp.png" alt="whatsapp" width="75" height="75" />
+          <p class="text-xl text-white">Whatsapp</p>
+        </a>
+        <a href="#" class="flex flex-col items-center space-y-[25px]">
+          <img src="../assets/images/facebook.png" alt="facebook" width="75" height="75" />
+          <p class="text-xl text-white">Facebook</p>
+        </a>
+      </div>
+    </footer>
+  </div>
+</template>
